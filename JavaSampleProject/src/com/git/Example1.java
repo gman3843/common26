27968632a02023cb32a98dfa2f209d6da68fd216
gman3843 <gsislove1@gmail.com>
2024-07-27 15:42:49 -0700

    First commit file.

---
+++ JavaSampleProject/src/com/git/Example1.java
@@ -0,0 +1,20 @@
+package com.git;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+		System.out.println("Hello Friends");
+	
+	}
+
+}
