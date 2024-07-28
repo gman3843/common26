9bba40f42ff9b019f5c735cdcc691cfb9de030a8
gman3843 <gsislove1@gmail.com>
2024-07-28 13:22:49 -0700

    I commit java program

---
+++ Sample1/src/com/git/language.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class language {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Java is an Object orinted language");
+		System.out.println("Java does not support pointers");
+		System.out.println("Java does not support pointers");
+	}
+
+}
